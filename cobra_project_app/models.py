d55b1da0dc3289e38660f697970cb323388233c7
Михаил <shishkanov.mv@phystech.edu>
2025-04-12 17:33:00 +0300

    Models for DB was created

--- cobra_project_app/models.py
+++ cobra_project_app/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import ForeignKey
 
-# Create your models here.
+
+class Test(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+
+class Question(models.Model):
+    test = ForeignKey(Test, on_delete=models.CASCADE, related_name='questions')
+    name = models.CharField(max_length=100)
+
+
+class Answer(models.Model):
+    question = ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    name = models.CharField(max_length=50)
+    correctness = models.BooleanField(default=False)
